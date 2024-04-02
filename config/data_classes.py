c4d540ec5a717f43e6da7f20ecbbb7d896b2d1fe
iknki <cy9556@gmail.com>
2024-04-02 23:08:26 +0800

    完善数据库操作

---
+++ config/data_classes.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+class Response(BaseModel):
+    status : int
+    message: str
+    token: str
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+class User(BaseModel):
+    Username: str
+    Password: str
+
+class ChatResponse(BaseModel):
+    status: int
+    messages: list[Message]
+
+
+class Session(BaseModel):
+    question: str
+
